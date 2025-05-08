235cfb5063ce1897e78cd556eaca649254ce4b07
blobunderscore <lolbomjnoob@gmail.com>
2025-05-08 19:03:01 +0600

    remove dsomethi

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import discord
 import random
 from discord.ext import commands
 from discord import app_commands
-from dotenv import load_dotenv
-
-load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
